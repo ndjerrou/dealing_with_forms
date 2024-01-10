da0881e19316aef103951369544d89bb325bfbb4
ndjerrou <n.djerrou@gmail.com>
2024-01-10 17:02:48 +0100

    state formula complete

--- src/component/AddUser.js
+++ src/component/AddUser.js
@@ -2,11 +2,19 @@ import { useState } from 'react';
 
 const AddUser = () => {
   const [firstName, setFirstName] = useState('');
+  const [age, setAge] = useState(0);
+  const [isMajor, setMajor] = useState(false);
 
   console.log(firstName);
+  console.log(age);
+  console.log(isMajor);
 
   const handleFirstName = e => {
     setFirstName(e.target.value);
+  };
+
+  const handleIsMajor = e => {
+    setMajor(e.target.checked);
   };
 
   return (
@@ -17,10 +25,14 @@ const AddUser = () => {
       </div>
       <div>
         <label>Age</label>
-        <input type='number' value='' />
+        <input
+          type='number'
+          value={age}
+          onChange={e => setAge(e.target.value)}
+        />
       </div>
       <div>
-        <input type='checkbox' value='' />
+        <input type='checkbox' checked={isMajor} onChange={handleIsMajor} />
         <label>Majeur ?</label>
       </div>
       <button>Add a user</button>
